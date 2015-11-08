1754634189132b73bd68bd77baeba12ef389c8e8
David Arias <arias1011@gmail.com>
2015-11-08 18:41:04 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,74 @@
+;; remove tools bar
+;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen
+(setq inhibit-startup-message t)
+
+;; how line numbers
+(global-linum-mode t)
+
+;; no anoying backup files
+(setq make-backup-files nil)
+
+;;automatic autosaves anoying #files# in tmp
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+;; save all buffer on autosave
+(defun full-auto-save ()
+  (interactive)
+  (save-excursion
+    (dolist (buf (buffer-list))
+      (set-buffer buf)
+      (if (and (buffer-file-name) (buffer-modified-p))
+	  (basic-save-buffer)))))
+(add-hook 'auto-save-hook 'full-auto-save)
+
+;; save on focus lost
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-hook 'focus-out-hook 'save-all)
+
+;; package manager
+(require 'package)
+
+;; set the list of packaes to install
+(setq package-list '(ac-js2 js2-mode auto-complete yasnippet))
+
+;; packages repositories
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; set color themes ( requires emacs-goodies package)
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-charcoal-black)
+
+;; use js2 mode in js files
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;; auto complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
